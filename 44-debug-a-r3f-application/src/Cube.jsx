a556cb00a03e104f3be05f909111f6646f2b5f6b
gin280 <zxy_wtfm@163.com>
2023-12-28 17:41:18 +0800

    feat: 44

---
+++ 44-debug-a-r3f-application/src/Cube.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Cube = () => {
+  return (
+    <mesh position-x={2} scale={1.5}>
+      <boxGeometry />
+      <meshStandardMaterial color="mediumpurple" />
+    </mesh>
+  )
+}
+
+export default Cube
---
+++ 44-debug-a-r3f-application/src/Experience.jsx
@@ -0,0 +1,25 @@
+import { OrbitControls } from "@react-three/drei"
+import Cube from "./Cube"
+
+export default function Experience() {
+  return (
+    <>
+      <OrbitControls makeDefault />
+
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
+
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <Cube />
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+    </>
+  )
+}
